be6dfc32a2881c6951683f42867c571bbccf8c56
Jeny Sams <jeniusam@gmail.com>
2021-02-09 14:22:18 +0300

    Fixed pep8

--- api/views.py
+++ api/views.py
@@ -4,10 +4,9 @@ from api_yamdb import settings
 
 from django.core.mail import send_mail
 from django.db.models import Avg
+from django.shortcuts import get_object_or_404
 
 from django_filters.rest_framework import DjangoFilterBackend
-
-from django.shortcuts import get_object_or_404
 
 from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import action
